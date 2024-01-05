f2123b12745fb1fdbcd8eb3d43a59b6629752f66
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2024-01-05 18:51:31 +0530

    Add files via upload

---
+++ About fragments/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+  return ( 
+  <React.Fragment> 
+    {/* <></> */}
+  <h1>Healthy Food</h1>
+  <ul class="list-group">
+  <li class="list-group-item">Dal</li>
+  <li class="list-group-item">Fish</li>
+  <li class="list-group-item">Green Vegetables</li>
+  <li class="list-group-item">Yogurt</li>
+  <li class="list-group-item">Fruits</li>
+</ul>
+  </React.Fragment>
+  )
+}
+export default App
